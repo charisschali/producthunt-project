e6b249a6afbdb78ae5275c73a97cb0de3ec0fe71
teezjay <teezjay11@gmail.com>
2018-05-29 21:53:54 +0300

    website complete

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length="1000")
+    url = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User,on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['-pub_date',]
+    #a function to return a portion of text
+    def summary(self):
+        return self.body[:50]
+    #print time without the actual time
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    #prints titlein django admin as an understable manner
+    def __str__(self):
+        return self.title
